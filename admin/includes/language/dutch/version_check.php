f48a2039bc93d3b4c99465bb4cde446c9c658f26
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2020-09-26 13:55:55 +0200

    1079
    1079

--- admin/includes/language/dutch/version_check.php
+++ admin/includes/language/dutch/version_check.php
@@ -21,7 +21,5 @@ define('TEXT_RELEASE_LINK', 'Aankondiging van release bekijken');
 define('TITLE_INSTALLED_VERSION', 'Geïnstalleerde versie:');
 
 define('VERSION_RUNNING_LATEST', 'U gebruikt de nieuwste versie van OSCOM CE Phoenix.');
-define('VERSION_UPGRADES_AVAILABLE', 'Er is een nieuwe versie beschikbaar om te downloaden! (OSCOM CE Phoenix v%s)');
-define('VERSION_UPGRADES_AVAILABLE', 'Er is een nieuwe versie beschikbaar om te downloaden! (OSCOM CE Phoenix v%s)<hr><a class="alert-link" target="_blank" href="https://forums.oscommerce.com/topic/494774-certified-service-providers-development-hosting-themes-and-more/">Gecertificeerde Developers</a> and detailed "<a class="alert-link" target="_blank" href="https://forums.oscommerce.com/tags/phoenix%%20update/">en gedetailleerde Doe het zelf </a> -instructies zijn beschikbaar in de Phoenix Club.');
-
+define('VERSION_UPGRADES_AVAILABLE', 'Er is een nieuwe versie beschikbaar om te downloaden! (CE Phoenix v%s)<hr><a class="alert-link" target="_blank" href="https://forums.oscommerce.com/topic/494774-certified-service-providers-development-hosting-themes-and-more/">Gecertificeerde Developers</a> en gedetailleerde "<a class="alert-link" target="_blank" href="https://forums.oscommerce.com/tags/phoenix%%20update/">Doe het zelf"</a> -instructies zijn beschikbaar in de Phoenix Club.');
 define('ERROR_COULD_NOT_CONNECT', 'Kan geen verbinding maken met de osCommerce-website om te controleren op nieuwe versies.');
