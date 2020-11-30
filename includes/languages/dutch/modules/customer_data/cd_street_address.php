43fef16026a1bb70f36fe22661e2eb9f605c266c
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2020-11-30 14:42:39 +0100

    Update cd_street_address.php
    Huisnummer rood vetgedrukt

--- includes/languages/dutch/modules/customer_data/cd_street_address.php
+++ includes/languages/dutch/modules/customer_data/cd_street_address.php
@@ -13,6 +13,6 @@
 const MODULE_CUSTOMER_DATA_STREET_ADDRESS_TEXT_TITLE = 'Adres';
 const MODULE_CUSTOMER_DATA_STREET_ADDRESS_TEXT_DESCRIPTION = 'Toon een adresveld in create acoount pagina';
 
-const ENTRY_STREET_ADDRESS = 'Adres + Huisnummer';
+const ENTRY_STREET_ADDRESS = 'Adres + <span class="text-danger font-weight-bolder">Huisnummer</span>';
 const ENTRY_STREET_ADDRESS_ERROR = 'Uw adres moet minimaal %d tekens bevatten.';
 const ENTRY_STREET_ADDRESS_TEXT = '';
