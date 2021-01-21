b72698fc7cb4480f5562c1d931df6391fe1857e9
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-01-21 16:07:42 +0100

    Update session_storage.php
    1.0.7.14

--- admin/includes/languages/dutch/modules/security_check/session_storage.php
+++ admin/includes/languages/dutch/modules/security_check/session_storage.php
@@ -12,4 +12,3 @@
 
 const WARNING_SESSION_DIRECTORY_NON_EXISTENT = 'De sessie directory bestaat niet:  "%s". Sessies werken pas als deze directory is aangemaakt.';
 const WARNING_SESSION_DIRECTORY_NOT_WRITEABLE = 'Ik kan niet schrijven naar de sessie directory:  "%s". Sessies werken pas als de juiste gebruikersrechten zijn ingesteld.';
-
