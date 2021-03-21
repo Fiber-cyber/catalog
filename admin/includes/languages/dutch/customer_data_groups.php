61510a1a84d79ea68584dda7dcbe582c4f6a5d52
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-03-21 11:41:03 +0100

    Update customer_data_groups.php
    Waarschuwing toegevoegd bij het verwijderen van een klantgegevensgroep.

--- admin/includes/languages/dutch/customer_data_groups.php
+++ admin/includes/languages/dutch/customer_data_groups.php
@@ -24,9 +24,13 @@ const TEXT_INFO_CUSTOMER_DATA_GROUP_NAME = 'Naam:';
 const TEXT_INFO_VERTICAL_SORT_ORDER = 'Verticale sorteer volgorde: %s';
 const TEXT_INFO_HORIZONTAL_SORT_ORDER = 'Horizontale sorteer volgorde: %s';
 const TEXT_INFO_WIDTH = 'Wijdte (1-12 twaalfde van het scherm): %s';
-const TEXT_INFO_INSERT_INTRO = 'Voer de nieuwe klantgegevens groep in met de bijbehorende gegevens';
-const TEXT_INFO_DELETE_INTRO = 'Weet u zeker dat u deze klantgegevens groep wilt verwijderen?';
+const TEXT_INFO_INSERT_INTRO = 'Voer de nieuwe klantgegevensgroep in met de bijbehorende gegevens';
+const TEXT_INFO_DELETE_INTRO = <<<'EOT'
+<p>Weet u zeker dat u deze klantgegevensgroep wilt verwijderen?</p>
+<p class="alert alert-warning">Warning: Als klantgegevensmodules deze groep gebruiken, worden ze wees als de groep wordt verwijderd!
+Overweeg of u deze groep beter kunt bewerken in plaats van deze te verwijderen en een nieuwe te maken.
+Om te vertalen, zou het in plaats daarvan normaal gesproken bewerken.</p>
+EOT;
 const TEXT_INFO_HEADING_NEW_CUSTOMER_DATA_GROUP = 'Nieuwe klantgegevens groep';
 const TEXT_INFO_HEADING_EDIT_CUSTOMER_DATA_GROUP = 'Bewerk klantgegevens groep';
 const TEXT_INFO_HEADING_DELETE_CUSTOMER_DATA_GROUP = 'Verwijder klantgegevens groep';
-//const TEXT_DISPLAY_NUMBER_OF_CUSTOMER_DATA_GROUPS = 'Aantal klantgegevens groepen';
