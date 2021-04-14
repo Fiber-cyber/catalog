89accca7ef63c7fe103c5b71b0fffb00d77ff3a6
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-04-14 13:09:50 +0200

    Update server_info.php
    Adds a class to manage system information to replace the functions
    used now.
    
    Modifies the admin/server_info.php to use the new class.
    
    Updates the language file to remove unused text and options.

--- admin/includes/languages/dutch/server_info.php
+++ admin/includes/languages/dutch/server_info.php
@@ -10,27 +10,23 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Server Informatie');
+const HEADING_TITLE = 'Server Informatie';
 
-define('TABLE_HEADING_KEY', 'Sleutel');
-define('TABLE_HEADING_VALUE', 'Waarde');
+const TABLE_HEADING_KEY = 'Sleutel';
+const TABLE_HEADING_VALUE = 'Waarde';
 
-define('TITLE_SERVER_HOST', 'Server Host');
-define('TITLE_SERVER_OS', 'Server OS');
-define('TITLE_SERVER_DATE', 'Server Datum');
-define('TITLE_SERVER_UP_TIME', 'Server Up Time');
-define('TITLE_HTTP_SERVER', 'HTTP Server');
-define('TITLE_PHP_VERSION', 'PHP Versie');
-define('TITLE_ZEND_VERSION', 'Zend');
-define('TITLE_DATABASE_HOST', 'Database Host');
-define('TITLE_DATABASE', 'Database');
-define('TITLE_DATABASE_DATE', 'Database Datum');
+const TITLE_SERVER_HOST = 'Server Host';
+const TITLE_SERVER_OS = 'Server OS';
+const TITLE_SERVER_DATE = 'Server Datum';
+const TITLE_SERVER_UP_TIME = 'Server Uptime';
+const TITLE_HTTP_SERVER = 'HTTP Server';
+const TITLE_PHP_VERSION = 'PHP Versie';
+const TITLE_ZEND_VERSION = 'Zend';
+const TITLE_DATABASE_HOST = 'Database Host';
+const TITLE_DATABASE = 'Database';
+const TITLE_DATABASE_DATE = 'Database Datum';
 
-define('TEXT_EXPORT_INTRO', 'De volgende informatie kan bij osCommerce worden ingediend door op de knop Verzenden te klikken. U kunt de informatie ook in een bestand opslaan door op opslaan te klikken. Deze informatie is volledig anoniem en kan niet worden gebruikt om een individueel systeem te identificeren. Het wordt alleen gebruikt voor ondersteunings- en ontwikkelingsdoeleinden.');
-define('TEXT_EXPORT_INFO', 'Export Server Informatie');
+const TEXT_EXPORT_INTRO = 'U kunt de informatie in een bestand opslaan door op Opslaan te klikken.';
+const TEXT_EXPORT_INFO = 'Export Server Informatie';
 
-define('SUCCESS_INFO_SUBMIT', 'Uw informatie is succesvol ingediend.');
-define('ERROR_INFO_SUBMIT', 'Kan geen verbinding maken met de osCommerce-website om uw configuratie in te dienen. Probeer het later opnieuw');
-
-define('BUTTON_SAVE_TO_DISK', 'Bewaar op uw harde schijf');
-define('BUTTON_SEND_TO_OSCOMMERCE', 'Verzenden naar osCommerce');
+const BUTTON_SAVE_TO_DISK = 'Bewaar op uw harde schijf';
