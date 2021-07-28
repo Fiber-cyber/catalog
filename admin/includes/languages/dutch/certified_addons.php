451a2ed968cccafee86c5ce8a54dc501ae4927e6
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-07-28 09:09:59 +0200

    Update certified_addons.php
    left over

--- admin/includes/languages/dutch/certified_addons.php
+++ admin/includes/languages/dutch/certified_addons.php
@@ -18,9 +18,3 @@ const TABLE_CERTIFIED_ADDONS_REMARKS = 'Opmerkingen';
 
 const TEXT_CERTIFIED_ADDONS = '<p class="m-0 lead">Alle Addons/Services die hier worden vermeld, zijn <strong>gecertificeerd</strong> door een lid van het Phoenix Core Team.</p>%s';
 const NUM_CERTIFIED_ADDONS = 'Er zijn momenteel <b>%d</b> Gecertificeerde Addons/Services.';
-/*
-const TEXT_CERTIFIED_SEARCH_LABEL = 'Zoeken:';
-const TEXT_CERTIFIED_SEARCH_PLACEHOLDER = 'start met typen...';
-
-const NUM_FILTERED_ADDONS = '<span class="filtered-result"><br>Gefilterd op <b>{X}</b> Addon(s).</span>';
-*/
