6ab03c97f28e211ab19567555b06670c0ea3d715
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-08-11 12:28:25 +0200

    add zones dutch and belgium
    add zones dutch and belgium

---
+++ zones_belgie.sql
@@ -0,0 +1,11 @@
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Antwerpen','Antwerpen');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Bruxelles','Bruxelles');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Hainaut','Hainaut');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Limburg','Limburg');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Li&eacute;ge','Li&eacute;ge');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Luxembourg','Luxembourg');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Namur','Namur');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Oost-Vlaanderen','Oost-Vlaanderen');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Vlaams-Brabant','Vlaams-Brabant');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'Brabant wallon','Brabant wallon');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (21,'West-Vlaanderen','West-Vlaanderen');
---
+++ zones_dutch.sql
@@ -0,0 +1,14 @@
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Drenthe','Drenthe');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Flevoland','Flevoland');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Friesland','Friesland');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Gelderland','Gelderland');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Groningen','Groningen');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Limburg','Limburg');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Noord-Brabant','Noord-Brabant');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Noord-Holland','Noord-Holland');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Overijssel','Overijssel');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Utrecht','Utrecht');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Zeeland','Zeeland');
+INSERT INTO zones (zone_country_id, zone_code, zone_name) VALUES (150,'Zuid_Holland','Zuid_Holland');
+
+
