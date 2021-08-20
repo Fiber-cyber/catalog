2e63fd916afa457b2f7a35881fd37d4ea2064605
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-08-20 10:42:07 +0200

    Update pm2checkout.php
    Modernize 2Checkout module

--- includes/languages/dutch/modules/payment/pm2checkout.php
+++ includes/languages/dutch/modules/payment/pm2checkout.php
@@ -2,18 +2,25 @@
 /*
   $Id$
 
-  CE Phoenix, E-Commerce made Easy
-  https://phoenixcart.org
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
 
-  Copyright (c) 2021 Phoenix Cart
+  Copyright (c) 2021 osCommerce
+  
   Released under the GNU General Public License
 */
-
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_TITLE', '2Checkout');
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_PUBLIC_TITLE', '2Checkout');
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_PUBLIC_DESCRIPTION', 'CKredietkaarten en alternatieven');
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_DESCRIPTION', '<i class="fas fa-external-link-alt"></i>&nbsp;<a href="https://www.2checkout.com/" target="_blank" rel="noreferrer">2Checkout Website</a><br><br>Krediet Kaart Test Info:<br><br>CC#: 4111111111111111<br>Vervalt: Elke');
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_ERROR_MESSAGE', 'Er is een fout opgetreden bij het verwerken van uw creditcard. Probeer het alstublieft opnieuw.');
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_WARNING_DEMO_MODE', 'Getest: transactie uitgevoerd in demomodus.');
-  define('MODULE_PAYMENT_2CHECKOUT_TEXT_WARNING_TRANSACTION_ORDER', 'Getest: transactietotaal kwam niet overeen met ordertotaal.');
-?>
+  
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_TITLE = '2Checkout';
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_PUBLIC_TITLE = '2Checkout';
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_PUBLIC_DESCRIPTION = 'CKredietkaarten en alternatieven';
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_DESCRIPTION = <<<'EOT'
+<i class="fas fa-external-link-alt"></i>&nbsp;<a href="https://www.2checkout.com/referral?r=b4a119de94" target="_blank" rel="noreferrer">2Checkout Website</a><br>
+<br>
+Credit Card Test Info:<br>
+<br>
+CC#: 4111111111111111<br>
+Expiry: Any
+EOT;
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_ERROR_MESSAGE = 'Er is een fout opgetreden bij het verwerken van uw creditcard. Probeer het alstublieft opnieuw.';
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_WARNING_DEMO_MODE = 'Getest: transactie uitgevoerd in demomodus.';
+  const MODULE_PAYMENT_2CHECKOUT_TEXT_WARNING_TRANSACTION_ORDER = 'Getest: transactietotaal kwam niet overeen met ordertotaal.';
