bac89cac8043c30c5c38c50624965348aa278735
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-09-19 13:56:11 +0200

    Update specials.php
    Don't allow visits to the specials page when no products

--- admin/includes/languages/dutch/specials.php
+++ admin/includes/languages/dutch/specials.php
@@ -10,28 +10,37 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Aanbiedingen');
+const HEADING_TITLE = 'Aanbiedingen';
 
-define('TABLE_HEADING_PRODUCTS', 'Artikelen');
-define('TABLE_HEADING_PRODUCTS_PRICE', 'Artikel Prijs');
-define('TABLE_HEADING_SPECIAL_PRICE', 'Aanbiedingsprijs');
-define('TABLE_HEADING_STATUS', 'Status');
-define('TABLE_HEADING_ACTION', 'Actie');
+const TABLE_HEADING_PRODUCTS = 'Artikelen';
+const TABLE_HEADING_PRODUCTS_PRICE = 'Artikel Prijs';
+const TABLE_HEADING_SPECIAL_PRICE = 'Aanbiedingsprijs';
+const TABLE_HEADING_STATUS = 'Status';
+const TABLE_HEADING_ACTION = 'Actie';
 
-define('TEXT_SPECIALS_PRODUCT', 'Artikel');
-define('TEXT_SPECIALS_SPECIAL_PRICE', 'Speciale prijs');
-define('TEXT_SPECIALS_EXPIRES_DATE', 'Vervaldatum');
-define('TEXT_SPECIALS_PRICE_TIP', '<p class="lead">Aanbiedingen Note:</p><dl class="row"><dt class="col-sm-3">Percentage</dt><dd class="col-sm-9">In het veld Speciale prijs kunt u bijvoorbeeld een percentage invoeren: <strong>20%</strong></dd><dt class="col-sm-3">Decimaal scheidingsteken</dt><dd class="col-sm-9">Als u een nieuwe prijs invoert, moet het decimale scheidingsteken een <strong>.</strong> zijn (decimaal-punt), voorbeeld: <strong>49.99</strong></dd><dt class="col-sm-3">Vervaldatum</dt><dd class="col-sm-9">Laat de vervaldatum leeg voor geen vervaldatum</dd></dl>');
+const TEXT_SPECIALS_PRODUCT = 'Artikel';
+const TEXT_SPECIALS_SPECIAL_PRICE = 'Speciale Prijs';
+const TEXT_SPECIALS_EXPIRES_DATE = 'Verval Datum';
+const TEXT_SPECIALS_PRICE_TIP = <<<'EOT'
+<p class="lead">Uitleg:</p>
+<dl class="row">
+<dt class="col-sm-3">Percentage</dt><dd class="col-sm-9">In het veld Speciale prijs kunt u bijvoorbeeld een percentage invoeren: <strong>20%</strong></dd>
+<dt class="col-sm-3">Decimal Separator</dt><dd class="col-sm-9">Als u een nieuwe prijs invoert, moet het decimale scheidingsteken een <strong>.</strong> zijn (decimal-punt), voorbeeld: <strong>49.99</strong></dd>
+<dt class="col-sm-3">Expiry Date</dt><dd class="col-sm-9">Laat de vervaldatum leeg voor geen vervaldatum</dd>
+</dl>
+EOT;
 
-define('TEXT_INFO_DATE_ADDED', 'Datum toegevoegd:');
-define('TEXT_INFO_LAST_MODIFIED', 'Laatst gewijzigd:');
-define('TEXT_INFO_NEW_PRICE', 'Nieuwe Prijs:');
-define('TEXT_INFO_ORIGINAL_PRICE', 'Originele prijs:');
-define('TEXT_INFO_PERCENTAGE', 'Percentage:');
-define('TEXT_INFO_EXPIRES_DATE', 'Verloopt op:');
-define('TEXT_INFO_STATUS_CHANGE', 'Status verandering:');
+const TEXT_INFO_DATE_ADDED = 'Datum toegevoegd:';
+const TEXT_INFO_LAST_MODIFIED = 'Laatst gewijzigd:';
+const TEXT_INFO_NEW_PRICE = 'Nieuwe Prijs:';
+const TEXT_INFO_ORIGINAL_PRICE = 'Originele Prijs:';
+const TEXT_INFO_PERCENTAGE = 'Percentage:';
+const TEXT_INFO_EXPIRES_DATE = 'Verloopt op:';
+const TEXT_INFO_STATUS_CHANGE = 'Status verandering:';
 
-define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Verwijder aanbieding');
-define('TEXT_INFO_DELETE_INTRO', 'Weet u het zeker dat u deze aanbieding wilt verwijderen?');
+const TEXT_INFO_HEADING_DELETE_SPECIALS = 'Verwijder aanbieding';
+const TEXT_INFO_DELETE_INTRO = 'Weet u het zeker dat u deze aanbieding wilt verwijderen?';
 
-define('BUTTON_INSERT_SPECIAL', 'Voeg een nieuwe aanbieding toe');
+const BUTTON_INSERT_SPECIAL = 'Voeg een nieuwe aanbieding toe';
+
+const WARNING_NO_PRODUCTS = 'U moet een artikel toevoegen voordat u het een aanbieding kan maken..';
