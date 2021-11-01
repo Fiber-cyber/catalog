bae012912cffe9903a6f7a1844ddda5ab2f3f3d0
Fiber-cyber <64646021+Fiber-cyber@users.noreply.github.com>
2021-11-01 16:16:58 +0100

    1.0.8.8
    1.0.8.8

--- admin/includes/languages/dutch/version_check.php
+++ admin/includes/languages/dutch/version_check.php
@@ -21,7 +21,7 @@ const TITLE_INSTALLED_VERSION = 'Geïnstalleerde versie: <strong>CE Phoenix v%s<
 const VERSION_SERVER_FAILURE = 'De beschikbare versies van de server kunnen niet worden geladen. Controleer uw internet verbinding of probeer het later opnieuw.';
 const VERSION_RUNNING_LATEST = 'U gebruikt de nieuwste versie van CE Phoenix.';
 const VERSION_UPGRADES_AVAILABLE = <<<'EOT'
-<strong>CE Phoenix %s</strong>is de laatste versie die voor u beschikbaar is!<hr>
+<strong>CE Phoenix %s</strong> is de laatste versie die voor u beschikbaar is!<hr>
 <a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/viewtopic.php?f=26&t=309">Gecertificeerde Developers</a>
  en gedetailleerde "<a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/app.php/tag/phoenix-update">Doe het zelf</a>"
  instructies zijn beschikbaar in de Phoenix Club.
