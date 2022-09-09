f9b1cd3b3720ae37e405ea36e891e75ebef82f61
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2022-09-09 10:49:49 +0200

    oke
    oke

--- admin/includes/languages/dutch/tax_rates.php
+++ admin/includes/languages/dutch/tax_rates.php
@@ -5,31 +5,31 @@
   CE Phoenix, E-Commerce made Easy
   https://phoenixcart.org
 
-  Copyright (c) 2021 Phoenix Cart
+  Copyright (c) 2022 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'BTW Tarieven');
+const HEADING_TITLE = 'BTW Tarieven';
 
-define('TABLE_HEADING_TAX_RATE_PRIORITY', 'Prioriteit');
-define('TABLE_HEADING_TAX_CLASS_TITLE', 'BTW Klas');
-define('TABLE_HEADING_COUNTRIES_NAME', 'land');
-define('TABLE_HEADING_ZONE', 'Zone');
-define('TABLE_HEADING_TAX_RATE', 'BTW Tarief');
-define('TABLE_HEADING_ACTION', 'Actie');
+const TABLE_HEADING_TAX_RATE_PRIORITY = 'Prioriteit';
+const TABLE_HEADING_TAX_CLASS_TITLE = 'BTW Klas';
+const TABLE_HEADING_COUNTRIES_NAME = 'Land';
+const TABLE_HEADING_ZONE = 'Zone';
+const TABLE_HEADING_TAX_RATE = 'BTW Tarief';
+const TABLE_HEADING_ACTION = 'Actie';
 
-define('TEXT_INFO_EDIT_INTRO', 'Breng de nodige wijzigingen aan');
-define('TEXT_INFO_DATE_ADDED', 'Datum toegevoegd: %s');
-define('TEXT_INFO_LAST_MODIFIED', 'Laatst gewijzigd: %s');
-define('TEXT_INFO_CLASS_TITLE', 'BTW Klas Titel:');
-define('TEXT_INFO_COUNTRY_NAME', 'Land:');
-define('TEXT_INFO_ZONE_NAME', 'Zone:');
-define('TEXT_INFO_TAX_RATE', 'BTW tarief (%):');
-define('TEXT_INFO_TAX_RATE_PRIORITY', 'Belastingtarieven met dezelfde prioriteit worden toegevoegd..<br /><br />Prioriteit:');
-define('TEXT_INFO_RATE_DESCRIPTION', 'Beschrijving:<br>%s');
-define('TEXT_INFO_INSERT_INTRO', 'Voer de nieuwe belastingtarief in met de bijbehorende gegevens');
-define('TEXT_INFO_DELETE_INTRO', 'Weet u het zeker dat u deze BTW Tarief wilt verwidjeren?');
-define('TEXT_INFO_HEADING_NEW_TAX_RATE', 'Nieuw BTW Tarief');
-define('TEXT_INFO_HEADING_EDIT_TAX_RATE', 'Bewerk BTW Tarief');
-define('TEXT_INFO_HEADING_DELETE_TAX_RATE', 'Verwijder BTW Tarief');
+const TEXT_INFO_EDIT_INTRO = 'Breng de nodige wijzigingen aan';
+const TEXT_INFO_DATE_ADDED = 'Datum toegevoegd: %s';
+const TEXT_INFO_LAST_MODIFIED = 'Laatst gewijzigd: %s';
+const TEXT_INFO_CLASS_TITLE = 'BTW klas titel:';
+const TEXT_INFO_COUNTRY_NAME = 'Land:';
+const TEXT_INFO_ZONE_NAME = 'Zone:';
+const TEXT_INFO_TAX_RATE = 'BTW Tarief (%):';
+const TEXT_INFO_TAX_RATE_PRIORITY = 'Belastingtarieven met dezelfde prioriteit worden toegevoegd..<br /><br />Prioriteit:';
+const TEXT_INFO_RATE_DESCRIPTION = 'Beschrijving:<br>%s';
+const TEXT_INFO_INSERT_INTRO = 'Voer de details in van hoe het belastingtarief in rekening moet worden gebracht';
+const TEXT_INFO_DELETE_INTRO = 'Weet u het zeker dat u deze BTW Tarief wilt verwidjeren?';
+const TEXT_INFO_HEADING_NEW_TAX_RATE = 'Nieuw BTW tarief';
+const TEXT_INFO_HEADING_EDIT_TAX_RATE = 'Bewerk BTW tarief';
+const TEXT_INFO_HEADING_DELETE_TAX_RATE = 'Verwijder BTW tarief';
