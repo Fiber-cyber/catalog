80f5fb21877f30913044c310dc767766a2fc025d
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2023-12-03 10:09:08 +0100

    Update customer_data_groups.php
    Sta toe dat de lay-out van klantgegevensgroepen wordt gemaakt vanuit Admin. Hierdoor kan de winkeleigenaar een mooie create_account-indeling creëren.

--- admin/includes/languages/dutch/customer_data_groups.php
+++ admin/includes/languages/dutch/customer_data_groups.php
@@ -13,17 +13,15 @@
 const HEADING_TITLE = 'Klantgegevens Groepen';
 
 const TABLE_HEADING_CUSTOMER_DATA_GROUP_NAME = 'Klantgegevens Groep';
-const TABLE_HEADING_SORT_ORDER_V = 'Verticale sorteer volgorde';
-const TABLE_HEADING_SORT_ORDER_H = 'Horizontale sorteer volgorde';
+const TABLE_HEADING_SORT_ORDER = 'Sorteer volgorde';
 const TABLE_HEADING_WIDTH = 'Wijdte';
 const TABLE_HEADING_ACTION = 'Actie';
 
 const TEXT_INFO_EDIT_INTRO = 'Breng de nodige wijzigingen aan';
 const TEXT_INFO_USE_FIRST_FOR_ALL = 'Controleer of u de eerste sorteervolgorde en breedtewaarden voor alle talen wilt gebruiken.';
 const TEXT_INFO_CUSTOMER_DATA_GROUP_NAME = 'Naam:';
-const TEXT_INFO_VERTICAL_SORT_ORDER = 'Verticale sorteer volgorde: %s';
-const TEXT_INFO_HORIZONTAL_SORT_ORDER = 'Horizontale sorteer volgorde: %s';
-const TEXT_INFO_WIDTH = 'Wijdte (1-12 twaalfde van het scherm): %s';
+const TEXT_INFO_SORT_ORDER = 'Sorteer volgorde: %s';
+const TEXT_INFO_WIDTH = 'Wijdte: %s';
 const TEXT_INFO_INSERT_INTRO = 'Voer de nieuwe klantgegevensgroep in met de bijbehorende gegevens';
 const TEXT_INFO_DELETE_INTRO = <<<'EOT'
 <p>Weet u zeker dat u deze klantgegevensgroep wilt verwijderen?</p>
