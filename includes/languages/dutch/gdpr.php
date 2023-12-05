dbcf0c3bb4504d307feecbd65f4d2b401e05a587
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2023-12-05 10:40:41 +0100

    Update gdpr.php
    Account link

--- includes/languages/dutch/gdpr.php
+++ includes/languages/dutch/gdpr.php
@@ -3,13 +3,14 @@
   $Id$
 
   CE Phoenix, E-Commerce made Easy
- https://phoenixcart.org
+  https://phoenixcart.org
 
   Copyright (c) 2021 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE', 'Data Privacy');
-define('HEADING_TITLE', 'Wat we van u weten...');
+const NAVBAR_TITLE_1 = 'Mijn Account';
+const NAVBAR_TITLE_2 = 'Data Privacy';
 
+const HEADING_TITLE = 'Wat we van u weten...';
