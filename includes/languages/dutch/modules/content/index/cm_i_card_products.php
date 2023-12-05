1d7db3cc4b9ae829058709b265b89849a672c533
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2023-12-05 10:41:32 +0100

    Update cm_i_card_products.php
    Verwijderd maand, toegevoegd bekijk alles

--- includes/languages/dutch/modules/content/index/cm_i_card_products.php
+++ includes/languages/dutch/modules/content/index/cm_i_card_products.php
@@ -3,14 +3,14 @@
   $Id$
   
   CE Phoenix, E-Commerce made Easy
- https://phoenixcart.org
+  https://phoenixcart.org
 
   Copyright (c) 2021 Phoenix Cart
   
   Released under the GNU General Public License
 */
-
   const MODULE_CONTENT_CARD_PRODUCTS_TITLE       = 'Nieuwe Artikelen';
   const MODULE_CONTENT_CARD_PRODUCTS_DESCRIPTION = 'Toont de module "Nieuwe producten" op uw Index pagina.';
   
-  const MODULE_CONTENT_CARD_PRODUCTS_HEADING     = 'Nieuwe artikelen voor %s';
+  const MODULE_CONTENT_CARD_PRODUCTS_HEADING     = 'Onze laatste artikelen';
+  const MODULE_CONTENT_CARD_PRODUCTS_VIEW_ALL    = 'Bekijk alles';
