acb6f6aea6f60c52432c81038aabd351d306777b
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-03-20 13:03:52 +0100

    Update account_notifications.php

--- includes/languages/dutch/account_notifications.php
+++ includes/languages/dutch/account_notifications.php
@@ -16,19 +16,19 @@ const NAVBAR_TITLE_2 = 'Artikel Notificaties';
 const HEADING_TITLE = 'Artikel Notificaties';
 
 const MY_NOTIFICATIONS_TITLE = 'Mijn Artikel Notificaties';
-const MY_NOTIFICATIONS_DESCRIPTION = 'Met de lijst met artikelmeldingen kunt u op de hoogte blijven van artikelen die u interessant vindt. <br> <br> Selecteer <strong>Artikelmeldingen </strong> om op de hoogte te blijven van alle artikel wijzigingen..';
+const MY_NOTIFICATIONS_DESCRIPTION = <<<'EOT'
+Met de artikelmeldingslijst kunt u op de hoogte blijven van artikelen die u interessant vindt.<br><br>
+Als u op de hoogte wilt blijven van alle artikelwijzigingen, selecteert u <strong>Algemene Artikel Notificaties</strong>.
+EOT;
 
 const GLOBAL_NOTIFICATIONS_TITLE = 'Algemene Artikel Notificaties';
-const MY_NOTIFICATIONS_DESCRIPTION = <<<'EOT'
-Met de lijst vam artikelmeldingen kunt u op de hoogte blijven van artikelen die u interessant vindt.<br><br>
-Om op de hoogte te blijven van alle artikelwijzigingen, selecteert u <strong>Algemene Artikel Notificaties</strong>.
-EOT;
+const GLOBAL_NOTIFICATIONS_DESCRIPTION = 'Ontvang meldingen over alle beschikbare artikelen.';
 
 const NOTIFICATIONS_TITLE = 'Artikel Notificaties';
-const NOTIFICATIONS_DESCRIPTION = 'Om een artikelmelding te verwijderen, schakelt u het selectievakje artikelen uit en klikt u op Doorgaan.';
+const NOTIFICATIONS_DESCRIPTION = 'Om een artikelnotificatie te verwijderen, schakelt u het selectievakje voor artikelen uit en klikt u op Doorgaan.';
 const NOTIFICATIONS_NON_EXISTING = <<<'EOT'
-Er zijn momenteel geen artikelen  gemarkeerd voor meldingen.<br><br>
-Om artikelen aan uw lijst met artikelmeldingen toe te voegen, klikt u op de notificatie-link die beschikbaar is op de pagina met gedetailleerde artikelinformatie.
+Er zijn momenteel geen artikelen gemarkeerd voor een notificatie.<br><br>
+Om artikelen aan uw lijst met notificatiemeldingen toe te voegen, klikt u op de notificatielink die beschikbaar is op de pagina met gedetailleerde artikelinformatie.
 EOT;
 
-const SUCCESS_NOTIFICATIONS_UPDATED = 'Uw artikelmeldingen zijn succesvol bijgewerkt.';
+const SUCCESS_NOTIFICATIONS_UPDATED = 'Uw artikelnotificaties zijn succesvol bijgewerkt.';
