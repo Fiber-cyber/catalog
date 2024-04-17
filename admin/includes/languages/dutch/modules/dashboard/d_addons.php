3e87342e23fd9fef17ad3b1619415ce3a255de44
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-04-17 07:59:39 +0200

    Create d_addons.php
    Laatste Addons

---
+++ admin/includes/languages/dutch/modules/dashboard/d_addons.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+  Copyright (c) 2024 Phoenix Cart
+  Released under the GNU General Public License
+*/
+
+const MODULE_ADMIN_DASHBOARD_ADDONS_TITLE = 'Laatste Addons';
+const MODULE_ADMIN_DASHBOARD_ADDONS_DESCRIPTION = 'Toon de nieuwste add-ons uit de Addons-bibliotheek';
+
+const MODULE_ADMIN_DASHBOARD_ADDONS_AUTHOR = 'Auteur';
+const MODULE_ADMIN_DASHBOARD_ADDONS_UPDATED = 'Bijgewerkt';
+const MODULE_ADMIN_DASHBOARD_ADDONS_VIEW_ALL = 'Ga naar de Add-ons Bibliotheek';
