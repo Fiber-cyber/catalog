864a0356191d8877ea428f50568c8c027202ab15
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-04-29 08:27:30 +0200

    1.0.9.2
    1.0.9.2

--- admin/includes/languages/dutch/manufacturers.php
+++ admin/includes/languages/dutch/manufacturers.php
@@ -51,6 +51,6 @@ const TEXT_EDIT_MANUFACTURERS_SEO_TITLE = 'Bewerk de Fabrikant Titel voor SEO:<b
 
 const BUTTON_INSERT_NEW_MANUFACTURER = 'Nieuwe Fabrikant';
 
-const TEXT_MANUFACTURERS_IMAGE_LABEL = 'Selecteer Fabrikant';
+const TEXT_MANUFACTURERS_IMAGE_LABEL = 'Selecteer Afbeelding';
 
 const GET_HELP_LINK = 'https://phoenixcart.org/phoenixcartwiki/index.php?title=Manufacturers';
