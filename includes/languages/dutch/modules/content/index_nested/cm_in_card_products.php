3d2c106458afd85d286c61b20d3587a7b818d119
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-05-07 16:06:20 +0200

    Nieuw
    Nieuw>Laatste

--- includes/languages/dutch/modules/content/index_nested/cm_in_card_products.php
+++ includes/languages/dutch/modules/content/index_nested/cm_in_card_products.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-  const MODULE_CONTENT_IN_CARD_PRODUCTS_TITLE       = 'Nieuwe Artikelen';
-  const MODULE_CONTENT_IN_CARD_PRODUCTS_DESCRIPTION = 'Toon nieuwe artikelen op uw index pagina.';
+  const MODULE_CONTENT_IN_CARD_PRODUCTS_TITLE       = 'Laatste Artikelen';
+  const MODULE_CONTENT_IN_CARD_PRODUCTS_DESCRIPTION = 'Toon de laatste artikelen op uw index pagina.';
   
-  const MODULE_CONTENT_IN_CARD_PRODUCTS_HEADING     = 'Nieuwe artikelen voor %s';
+  const MODULE_CONTENT_IN_CARD_PRODUCTS_HEADING     = 'Laatste artikelen';
