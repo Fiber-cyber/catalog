7c13756bec7cbabfd7d1918f962775788c218876
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-06-07 14:29:36 +0200

    Update store_logo.php
    introduce favicon

--- admin/includes/languages/dutch/store_logo.php
+++ admin/includes/languages/dutch/store_logo.php
@@ -12,12 +12,19 @@
 
 const HEADING_TITLE = 'Winkel logo';
 
-const TEXT_EXISTING_LOGO = '<b>Dit is het bestaande winkellogo.</b><br><br>Het wordt zowel in de shop als in de beheerdersgedeelte gebruikt (bijvoorbeeld op uw factuur en pakbon).';
+const TEXT_EXISTING_STORE_LOGO = '<b>Dit is het bestaande winkellogo.</b><br><br>Het wordt zowel in de shop als in de beheerdersgedeelte gebruikt (bijvoorbeeld op uw factuur en pakbon).';
+const TEXT_EXISTING_MINI_LOGO = '<b>Dit is uw bestaande mini-logo.</b><br><br>Het wordt mogelijk gebruikt in de navigatiebalk van uw winkel (afhankelijk van de module die wordt geïnstalleerd).';
+
+const TEXT_EXISTING_FAVICON_LOGO = '<b>Dit is uw bestaande Favicon.</b><br><br>Het wordt gebruikt wanneer een gebruiker uw site in zijn bladwijzers opslaat.';
 
 const BUTTON_EDIT_LOGO = 'Upload een nieuw logo';
+const BUTTON_EDIT_MINI_LOGO = 'Nieuw Mini Logo';
+const BUTTON_EDIT_FAVICON_LOGO = 'Nieuw Favicon';
 
-const TEXT_LOGO_IMAGE = 'Nieuw logo';
-const TEXT_FORMAT_AND_LOCATION = '<b>Bestaande logo wordt overschreven.</b><br><br>Het nieuwe winkellogo moet een standaard afbeeldingsformaat hebben: png/gif/jpg/svg/webp';
+const TEXT_LOGO_IMAGE = 'Selecteer een plaatje';
+const TEXT_FORMAT = 'Waarschuwing: de bestaande afbeelding kan overschreven worden.';
+const TEXT_LOCATION = 'De nieuwe afbeelding moet één van de volgende formaten zijn png/gif/jpg/svg/webp.';
+const TEXT_LOCATION_FAVICON = 'Voor de beste resultaten moet de nieuwe afbeelding een .png en vierkant zijn en minimaal 300px bij 300px groot zijn.';
 
 const SUCCESS_LOGO_UPDATED = '<strong>Succes:</strong> De Shop logo is succesvol bijgewerkt!';
 
