0d7e479fc9656a60fed354ca2602b91904d6d201
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-06-28 12:31:23 +0200

    Create nb_search.php
    Introductie zoeken in header

---
+++ includes/languages/dutch/modules/navbar/nb_search.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  $Id$
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+  Copyright (c) 2024 Phoenix Cart
+  Released under the GNU General Public License
+*/
+
+  const MODULE_NAVBAR_SEARCH_TITLE = 'Zoeken';
+  const MODULE_NAVBAR_SEARCH_DESCRIPTION = 'Toon zoekpictogram in navigatiebalk. <div class="alert alert-warning">Hierdoor wordt een pop-upvenster geopend met een zoekvak.</div>';
+
+  const MODULE_NAVBAR_SEARCH_PUBLIC_TEXT = '<i title="Zoek op onze site" class="fas fa-magnifying-glass fa-fw"></i><span class="d-inline d-sm-none"> Zoek op onze site..</span>';
