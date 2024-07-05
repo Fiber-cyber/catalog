5d822a52b913a275911dcd7a97db461e0b9af57d
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-07-05 09:42:12 +0200

    Update zones.php
    %d verandert het gewicht in een geheel getal, wat resulteert in een weergave van 0. veranderd in %s

--- includes/languages/dutch/modules/shipping/zones.php
+++ includes/languages/dutch/modules/shipping/zones.php
@@ -12,6 +12,6 @@
 
 const MODULE_SHIPPING_ZONES_TEXT_TITLE = 'Zone tarieven';
 const MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION = 'Op zone gebaseerd tarieven';
-const MODULE_SHIPPING_ZONES_TEXT_WAY = 'Verzenden naar %s : %d kg(s)';
+const MODULE_SHIPPING_ZONES_TEXT_WAY = 'Verzenden naar %s : %s kg(s)';
 const MODULE_SHIPPING_ZONES_INVALID_ZONE = 'Geen verzending beschikbaar naar het geselecteerde land';
 const MODULE_SHIPPING_ZONES_UNDEFINED_RATE = 'Het verzendtarief kan op dit moment niet worden bepaald';
