58ae9aef651318c3524e0d0bc262cd728e434f11
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-07-13 09:26:44 +0200

    1.0.9.5
    1.0.9.5

--- admin/includes/languages/dutch/login.php
+++ admin/includes/languages/dutch/login.php
@@ -15,7 +15,7 @@ const HEADING_TITLE = '<i class="fa fa-lock"></i> Veilig Inloggen';
 const TEXT_USERNAME = 'Gebruikersnaam';
 const TEXT_PASSWORD = 'Wachtwoord';
 
-const TEXT_CREATE_FIRST_ADMINISTRATOR = '<div class="card-body"><div class="alert alert-danger card-text">Er zijn geen beheerders in de databasetabel. Gebruik deze pagina om de eerste beheerder aan te maken.<br><strong>Na deze stap is een handmatige login nog steeds vereist.</strong></div></div>';
+const TEXT_CREATE_FIRST_ADMINISTRATOR = 'Er zijn geen beheerders gevonden in de database. Gebruik deze pagina om de eerste beheerder aan te maken.<<br><strong>Na deze stap is een handmatige login nog steeds vereist.</strong>';
 
 const ERROR_INVALID_ADMINISTRATOR = '<strong>FOUT:</strong> Ongeldige inlogpoging van de beheerder.';
 
