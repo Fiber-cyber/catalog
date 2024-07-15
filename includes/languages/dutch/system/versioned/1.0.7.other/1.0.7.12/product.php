ffdd69ed73070962388a6e8e3d905b9ee51f24e2
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-07-15 12:57:58 +0200

    Update product.php
    Tekst Verwijderd

--- includes/languages/dutch/system/versioned/1.0.7.other/1.0.7.12/product.php
+++ includes/languages/dutch/system/versioned/1.0.7.other/1.0.7.12/product.php
@@ -11,6 +11,6 @@
 */
 
 const IS_PRODUCT_SHOW_PRICE = '%s';
-const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<del>%s</del> <span class="text-danger">nu %s</span>';
+const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<del>%s</del> <span class="text-danger">%s</span>';
 const IS_PRODUCT_BUTTON_BUY = '<i class="fas fa-shopping-cart" title="Voeg toe aan de winkelwagen"></i>';
 const IS_PRODUCT_BUTTON_VIEW = '<i class="fas fa-eye" title="Bekijk artikel"></i> Bekijk';
