62a0489387fb1db3b91742c4e40b27dea7976e18
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-07-19 15:52:53 +0200

    Contact ons Layout

---
+++ includes/languages/dutch/modules/pi/contact_us/cu_form.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const CU_FORM_TITLE         = 'Formulier';
+  const CU_FORM_DESCRIPTION   = 'Toon het contactformulier op de pagina.<div class="alert alert-info">Dit is een child-module voor gebruik met het &pi; systeem.</div>';
+  
+  const FORM_CONTACT_US = '';
+  
+  const FORM_CONTACT_US_SUCCESS = '<div class="alert alert-success"><b>Dank u wel.</b> Uw aanvraag is verzonden naar de winkeleigenaar.<br><br>Als u ons nog een e-mail wilt sturen, <a class="alert-link" href="%s">klik dan hier</a>.</div>';
+  
