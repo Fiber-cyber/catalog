36bdca71edfbf6134b7aa77f97ccdfb6c8386df4
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-07-23 10:09:00 +0200

    1.0.9.5
    1.0.9.5

---
+++ includes/languages/dutch/modules/content/contact_us/cm_cu_modular.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  $Id$
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+  Copyright (c) 2024 Phoenix Cart
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_CU_MODULAR_TITLE        = '&pi; Modulair contact_ons';
+  const MODULE_CONTENT_CU_MODULAR_DESCRIPTION  = 'Nu kunt u uw contactpagina eenvoudiger indelen.<div class="alert alert-warning">Deze module vereist het gebruik van &pi; child-modules. Alle slots die geen onderliggende modules hebben...worden niet weergegeven!</div>';
+  
--- includes/languages/dutch/modules/customer_data/cd_matc.php
+++ includes/languages/dutch/modules/customer_data/cd_matc.php
@@ -13,10 +13,11 @@
 const MODULE_CUSTOMER_DATA_MATC_TEXT_TITLE = 'MATC';
 const MODULE_CUSTOMER_DATA_MATC_TEXT_DESCRIPTION = 'Instemming met de algemene voorwaarden vereisen.';
 
-
 // for the page(s)
 const ENTRY_MATC = 'Juridische overeenkomsten';
 const ENTRY_MATC_TEXT = 'Door de box aan te klikken, gaat u akkoord met onze <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#TCModal">Algemene Voorwaarden</a> en <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#PModal">Privacy</a> beleid.'; 
 
 // for the modal popup
 const MATC_BUTTON_CLOSE = 'Sluiten';
+
+const ENTRY_MATC_ERROR = 'U moet akkoord gaan met onze Algemene Voorwaarden om onze site te kunnen blijven gebruiken.';
