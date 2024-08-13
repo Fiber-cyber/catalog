8feb816e268d425750b7f6d7704fcaf49751c67c
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-08-13 15:44:10 +0200

    Update n_create_account.php
    Tekst was out of date

--- includes/languages/dutch/modules/notifications/n_create_account.php
+++ includes/languages/dutch/modules/notifications/n_create_account.php
@@ -14,7 +14,7 @@ const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_TITLE = 'Maak een profiel';
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_DESCRIPTION = 'Stuur een melding wanneer het aanmaken van een profiel is voltooid.';
 
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_SUBJECT = 'Welkom bij ' . STORE_NAME;
-const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = 'We verwelkomen u bij <strong>' . STORE_NAME . '</strong>.' . "\n\n";
-const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT = 'U kunt nu deelnemen aan de <strong> verschillende services</strong> die we u te bieden hebben. Sommige van deze services zijn:' . "\n\n" . '<li><strong>Permanente Winkelwagen</strong> - Alle artikelen die aan uw online winkelwagentje zijn toegevoegd, blijven daar totdat u ze verwijderd of uitcheckt.' . "\n" . '<li><strong>Adres Boek</strong> - We kunnen uw bestelde artikelen nu leveren op een ander adres dan het uwe! Dit is perfect om verjaardagscadeautjes rechtstreeks naar de jarige zelf te sturen.' . "\n" . '<li><strong>Order Historie</strong> - Bekijk uw geschiedenis van aankopen die u bij ons heeft gedaan.' . "\n" . '<li><strong>Artikel Recensies</strong> - Deel uw mening over artikelen met onze andere klanten.' . "\n\n";
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = '';
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT = 'U heeft uw gegevens succesvol bij ons geregistreerd.' . "\n\n" . 'De volgende keer dat u bij ons winkelt, meldt u zich gewoon aan voor een snellere afhandeling.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_CONTACT = 'Voor hulp bij een van onze online services kunt u een e-mail sturen naar de winkeleigenaar: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WARNING = '<strong>Note:</strong> Dit e-mailadres is aan ons gegeven door een van onze klanten. Als u zich niet heeft aangemeld als klant, stuur dan een e-mail naar ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n";
