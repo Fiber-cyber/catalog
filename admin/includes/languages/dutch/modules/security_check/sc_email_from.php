341449e5af81732bc4be22f4b3d4c0ae8a08fa5e
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-09-07 09:12:04 +0200

    Create sc_email_from.php
    E-mail "van" check

---
+++ admin/includes/languages/dutch/modules/security_check/sc_email_from.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const MODULE_SECURITY_CHECK_EMAIL_FROM_TITLE = 'e-mail van';
+const MODULE_SECURITY_CHECK_EMAIL_FROM_ERROR = <<<'EOT'
+U moet "E-mail van" instellen<br>
+Beheerder > E-mailopties > E-mail van<br>
+Dit e-mailadres moet hetzelfde domein zijn als uw winkel en een werkende inbox hebben (ingesteld door u of uw host).
+Bijvoorbeeld als uw winkel www.xyzshop.com is, kan uw e-mailadres info@xyzshop.com zijn
+EOT;
