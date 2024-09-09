f613a6855c510f30808adae7df2ab20ad32b1a60
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-09-09 12:02:40 +0200

    Update pm2checkout.php
    Update tekst en verwijzing ID

--- includes/languages/dutch/modules/payment/pm2checkout.php
+++ includes/languages/dutch/modules/payment/pm2checkout.php
@@ -14,12 +14,18 @@
   const MODULE_PAYMENT_2CHECKOUT_TEXT_PUBLIC_TITLE = '2Checkout';
   const MODULE_PAYMENT_2CHECKOUT_TEXT_PUBLIC_DESCRIPTION = 'CKredietkaarten en alternatieven';
   const MODULE_PAYMENT_2CHECKOUT_TEXT_DESCRIPTION = <<<'EOT'
-<i class="fas fa-external-link-alt"></i>&nbsp;<a href="https://www.2checkout.com/referral?r=b4a119de94" target="_blank" rel="noreferrer">2Checkout Website</a><br>
-<br>
-Credit Card Test Info:<br>
-<br>
-CC#: 4111111111111111<br>
-Expiry: Any
+<div class="alert alert-info">
+  Als u nog geen 2checkout-account heeft, kunt u zich veilig aanmelden via <a class="alert-link" href="https://www.2checkout.com/referral?r=ffc78bd5c5" target="_blank" rel="noreferrer">deze link</a>.
+  <hr>
+  Als u deze link gebruikt, ontvangt Phoenix eenmalig een gratis voordeel, zonder dat het u iets kost.
+</div>
+<h6>Credit Card Test Info</h6>
+<dl class="row no-gutters">
+  <dt class="col-sm-4">CC#</dt>
+  <dd class="col-sm-8">4111111111111111</dd>
+  <dt class="col-sm-4">Vervaldatum</dt>
+  <dd class="col-sm-8">Elke</dd>
+</dl>
 EOT;
   const MODULE_PAYMENT_2CHECKOUT_TEXT_ERROR_MESSAGE = 'Er is een fout opgetreden bij het verwerken van uw creditcard. Probeer het alstublieft opnieuw.';
   const MODULE_PAYMENT_2CHECKOUT_TEXT_WARNING_DEMO_MODE = 'Getest: transactie uitgevoerd in demomodus.';
