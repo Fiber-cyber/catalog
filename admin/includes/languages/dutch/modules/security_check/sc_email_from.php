0d38499a00cddf2774d885f8d876a529c8f77daf
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-09-10 15:05:39 +0200

    Update sc_email_from.php
    from

--- admin/includes/languages/dutch/modules/security_check/sc_email_from.php
+++ admin/includes/languages/dutch/modules/security_check/sc_email_from.php
@@ -12,8 +12,8 @@
 
 const MODULE_SECURITY_CHECK_EMAIL_FROM_TITLE = 'e-mail van';
 const MODULE_SECURITY_CHECK_EMAIL_FROM_ERROR = <<<'EOT'
-U moet "E-mail van" instellen<br>
-Beheerder > E-mailopties > E-mail van<br>
+U moet "E-mail from" instellen<br>
+Beheerder > E-mailopties > E-mail from<br>
 Dit e-mailadres moet hetzelfde domein zijn als uw winkel en een werkende inbox hebben (ingesteld door u of uw host).
 Bijvoorbeeld als uw winkel www.xyzshop.com is, kan uw e-mailadres info@xyzshop.com zijn
 EOT;
