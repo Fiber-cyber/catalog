a9f1431adcb801d617cc4d9671674c8a694564b7
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-09-27 08:51:10 +0200

    Create cm_gdpr_acceptance_data.php
    Opgeslagen overeenkomsten introductie

---
+++ includes/languages/dutch/modules/content/gdpr/cm_gdpr_acceptance_data.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_DATA_TITLE = 'GDPR/AVG Acceptatiegegevens';
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_DATA_DESCRIPTION = 'Toon klanten het winkelbeleid waarmee ze akkoord zijn gegaan.';
+
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_DATA_PUBLIC_TITLE = '%s';
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_DATA_EXPLANATION = '<small>Onze %s die u het laatst heeft geaccepteerd op %s.</small>';
+
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_TERMS = 'Beleidsdocument';
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_LANGUAGE = 'Beleidstaal';
+  
+  const MODULE_CONTENT_GDPR_ACCEPTANCE_TERMS_VIEW_BUTTON = 'Bekijk';
+  
