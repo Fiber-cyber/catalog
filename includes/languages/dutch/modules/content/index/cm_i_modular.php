b1c668228f096183e375107947988843ca678afa
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-10-13 09:05:26 +0200

    Create cm_i_modular.php
    Eenvoudig Indexpagina instellen

---
+++ includes/languages/dutch/modules/content/index/cm_i_modular.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_I_MODULAR_TITLE        = '&pi; Modulair index';
+  const MODULE_CONTENT_I_MODULAR_DESCRIPTION  = 'Nu kunt u uw indexpagina eenvoudiger indelen.<div class="alert alert-warning">Deze module vereist het gebruik van &pi;-child-modules. Alle slots die geen child-modules hebben...worden niet weergegeven!</div>';
