6c4ec9d0a04e93bfcce30945f13b4e6342ae99cf
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-10-22 09:02:07 +0200

    Create i_brand_icons.php
    Introductie Merk Icon/Afb

---
+++ includes/languages/dutch/modules/pi/index/i_brand_icons.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const I_BRAND_ICONS_TITLE = 'Merk Afbeelding/Icoon';
+  const I_BRAND_ICONS_DESCRIPTION = 'Geef merkiconen weer met een link naar de relevante pagina.<div class="alert alert-info">Dit is een child module voor gebruik met het &Pi; systeem.</div>';
+  
+  const I_BRAND_ICONS_HEADING     = 'Populaire Merken';
