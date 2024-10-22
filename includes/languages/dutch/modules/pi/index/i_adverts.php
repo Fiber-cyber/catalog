ca2c953d78aa38050bd0985bdb29fe26bbe06b34
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-10-22 11:48:16 +0200

    Introductie PI Advertentie/Hero module
    Introductie PI Advertentie/Hero module

---
+++ includes/languages/dutch/modules/pi/index/i_adverts.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const I_ADVERTS_TITLE        = 'Advertentie';
+  const I_ADVERTS_DESCRIPTION  = 'Advertentie naast elkaar.<div class="alert alert-info">Dit is een child module voor gebruik met het &Pi; system, welke linkt naar de Advertentie Manager.</div>';
---
+++ includes/languages/dutch/modules/pi/index/i_hero.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const I_HERO_TITLE       = 'Hero Tekst';
+  const I_HERO_DESCRIPTION = 'Tekst weergeven in een Hero Layout.<div class="alert alert-info">Dit is een child module voor gebruik met het &pi; systeem.</div>';
+
+  const I_HERO_TEXT = <<<EOT
+<h1 class="text-body-emphasis">#yourphoenix #yourcommunity</h1>
+<p class="col-lg-8 mx-auto fs-5 text-muted">
+  Maak er een snapshot van, tag het, deel het. Maak het eigen.
+</p>
+EOT;
+
+  const I_HERO_BUTTONS = <<<EOT
+<a href="%s" class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" role="button">Testimonials</a>
+EOT;
