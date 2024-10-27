1db9c04afc4805ce54df49ad917d61dad7dafde8
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-10-27 16:57:53 +0100

    Vertaling
    Betere vertaling

--- includes/languages/dutch/password_reset.php
+++ includes/languages/dutch/password_reset.php
@@ -20,4 +20,4 @@ const TEXT_MAIN = 'Voer een nieuw wachtwoord in voor uw profiel.';
 const TEXT_NO_RESET_LINK_FOUND = 'Fout: De link voor het opnieuw instellen van het wachtwoord is niet gevonden in onze gegevens. Probeer het opnieuw door een nieuwe link te genereren.';
 const TEXT_NO_EMAIL_ADDRESS_FOUND = 'Fout: Het e-mailadres is niet gevonden in onze administratie, probeer het opnieuw.';
 
-const SUCCESS_PASSWORD_RESET = 'Uw wachtwoord is succesvol geupdat&euml;t. Log in met uw nieuwe wachtwoord.';
+const SUCCESS_PASSWORD_RESET = 'Uw wachtwoord is succesvol Geüpdatet. Log in met uw nieuwe wachtwoord.';
