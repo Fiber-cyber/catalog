3630a2e317e699b6401cffa2d4f6b583400d274f
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-10-31 08:36:11 +0100

    Create pi_date_available.php
    Introductie Datum Beschikbaar PI-Layoutmodule

---
+++ includes/languages/dutch/modules/pi/product_info/pi_date_available.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const PI_DATE_AVAILABLE_TITLE       = 'Datum Beschikbaar';
+  const PI_DATE_AVAILABLE_DESCRIPTION = 'Geeft de toekomstige beschikbare datum weer (indien ingesteld) op de artikel-infopagina.';
+
+  const PI_DATE_AVAILABLE_TEXT        = 'Beschikbaar vanaf:<span class="badge badge-primary badge-pill text-bg-info">%s</span>';
