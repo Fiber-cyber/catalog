b3348dbc395c2389ff67cd76015d5aa8073e2687
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-11-03 08:33:27 +0100

    1.0.9.9
    Toegankelijkheids Optimalisaties

---
+++ templates/bootstrap5/includes/languages/dutch/create_account.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const NAVBAR_TITLE = 'Maak een profiel aan';
+
+const HEADING_TITLE = 'Maak een profiel aan';
+
+const TEXT_ORIGIN_LOGIN = '<strong>NOTE:</strong> Als u al een profiel heeft aangemaakt, kunt u uw gegevens op onze website raadplegen. <a class="alert-link" href="%s"><u>aanmeld pagina</u></a>.';
