ecd38b5fe0c226d8b9282b3ab074dcd829b9ca27
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-11-06 08:39:18 +0100

    Toegankelijkheid
    Toegankelijkheid

---
+++ templates/bootstrap5/includes/languages/dutch/address_book.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2022 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const NAVBAR_TITLE_1 = 'Mijn Profiel';
+const NAVBAR_TITLE_2 = 'Adres Boek';
+
+const HEADING_TITLE = 'Mijn Persoonlijke Adres Boek';
+
+const PRIMARY_ADDRESS_TITLE = 'Hoofdadres';
+const PRIMARY_ADDRESS_DESCRIPTION = <<<'EOT'
+This address is used as the pre-selected delivery and billing address for orders placed on this store and as the address for product and service tax calculations.
+EOT;
+
+const ADDRESS_BOOK_TITLE = 'Adres boekvermeldingen';
+
+const PRIMARY_ADDRESS = '(hoofd adres)';
+
+const TEXT_MAXIMUM_ENTRIES = '<span class="text-danger-emphasis">NOTE:</span> Een maximum van %s adressen toegestaan.';
