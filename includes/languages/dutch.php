5ce06171ec422708223c4c228230a3bcf0beb6af
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-11-28 12:43:02 +0100

    1.0.9.10
    1.0.9.10

--- includes/languages/dutch.php
+++ includes/languages/dutch.php
@@ -8,7 +8,7 @@
 
   Released under the GNU General Public License
   
-  Vertaald door Fiber - Phoenix 1.0.9.9
+  Vertaald door Fiber - Phoenix 1.0.9.10
 */
 
 // look in your $PATH_LOCALE/locale directory for available locales
