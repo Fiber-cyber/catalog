189daaa0164ad56c06ff1cf9f6253929d7389a73
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2024-12-15 12:18:57 +0100

    Update n_create_account.php
    Typo

--- includes/languages/dutch/modules/notifications/n_create_account.php
+++ includes/languages/dutch/modules/notifications/n_create_account.php
@@ -14,7 +14,7 @@ const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_TITLE = 'Maak een profiel';
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_DESCRIPTION = 'Stuur een melding wanneer het aanmaken van een profiel is voltooid.';
 
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_SUBJECT = 'Welkom bij ' . STORE_NAME;
-const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = 'Welcome bij <strong>' . STORE_NAME . '</strong>.' . "\n\n";
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = 'Welkom bij <strong>' . STORE_NAME . '</strong>.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT = 'U heeft uw gegevens succesvol bij ons geregistreerd.' . "\n\n" . 'De volgende keer dat u bij ons winkelt, meldt u zich gewoon aan voor een snellere afhandeling.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_CONTACT = 'Voor hulp bij een van onze online services kunt u een e-mail sturen naar de winkeleigenaar: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
 const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WARNING = '<strong>Note:</strong> Dit e-mailadres is aan ons gegeven door een van onze klanten. Als u zich niet heeft aangemeld als klant, stuur dan een e-mail naar ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n";
