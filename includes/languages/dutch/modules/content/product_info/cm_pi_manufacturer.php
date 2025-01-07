a42cd4ad39cb9dae36a2b72927838d894648a14b
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2025-01-07 13:25:50 +0100

    Create cm_pi_manufacturer.php
    Fabrikant/Importeur

---
+++ includes/languages/dutch/modules/content/product_info/cm_pi_manufacturer.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2024 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_PI_MANUFACTURER_TITLE       = 'Fabrikant/Importeur';
+  const MODULE_CONTENT_PI_MANUFACTURER_DESCRIPTION = 'Toon de Fabrikant (en Importeur indien ingesteld) in de product_info pagina.';
+  
+  const MODULE_CONTENT_PI_MANUFACTURER_BRAND = 'Fabrikant';
+  const MODULE_CONTENT_PI_MANUFACTURER_IMPORTER = 'Importeur';
+  
