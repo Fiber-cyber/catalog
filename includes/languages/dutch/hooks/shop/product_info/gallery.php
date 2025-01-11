a98ee065b3133249a2cf7a4768b93c5309e0ff6b
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2025-01-11 07:55:18 +0100

    Update gallery.php
    gallery

--- includes/languages/dutch/hooks/shop/product_info/gallery.php
+++ includes/languages/dutch/hooks/shop/product_info/gallery.php
@@ -18,3 +18,9 @@ const GALLERY_INDICATORS = 'Uit';
 const GALLERY_MODAL_SIZE = 'modal-md';
 const GALLERY_ALBUM_NAME = 'Album voor %s';
 const GALLERY_ALBUM_CLOSE = 'Sluiten';
+
+
+const GALLERY_NEXT_ITEM = 'Volgende';
+const GALLERY_PREV_ITEM = 'Vorige';
+
+const GALLERY_TO_ITEM = 'Plaatje %s';
