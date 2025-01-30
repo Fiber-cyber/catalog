0cd8a61d348bf914ae165ead82c2d10aa2b6b9c8
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2025-01-30 14:08:22 +0100

    Update cm_create_account_link.php
    punt

--- includes/languages/dutch/modules/content/login/cm_create_account_link.php
+++ includes/languages/dutch/modules/content/login/cm_create_account_link.php
@@ -14,5 +14,5 @@ const MODULE_CONTENT_CREATE_ACCOUNT_LINK_TITLE = 'Profiellink maken';
 const MODULE_CONTENT_CREATE_ACCOUNT_LINK_DESCRIPTION = 'Toon een aangemaakte profielcontainer op de inlogpagina';
 
 const MODULE_CONTENT_LOGIN_HEADING_NEW_CUSTOMER = 'Nieuwe klant';
-const MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER = 'Ik ben een nieuwe klant.';
+const MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER = 'Ik ben een nieuwe klant';
 const MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER_INTRODUCTION = 'Door een profiel aan te maken op ' . STORE_NAME . ' kunt u sneller winkelen, op de hoogte blijven van de status van een bestelling en de bestellingen die u eerder heeft gedaan bijhouden.';
