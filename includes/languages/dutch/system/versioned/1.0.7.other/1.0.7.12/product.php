bd4e643bfdaeeac23efbee7948700a68fafc906a
Fiber-cyber <mail@limshakei.nl>
2025-03-14 08:34:26 +0100

    Update product.php
    CSS Selectors

--- includes/languages/dutch/system/versioned/1.0.7.other/1.0.7.12/product.php
+++ includes/languages/dutch/system/versioned/1.0.7.other/1.0.7.12/product.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-const IS_PRODUCT_SHOW_PRICE = '%s';
-const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<span class="sr-only visually-hidden">Orginele Prijs</span><s>%s</s> <span class="sr-only visually-hidden">Verkoop prijs</span><span class="text-danger">%s</span>';
+const IS_PRODUCT_SHOW_PRICE = '<span class="productPrice">%s</span>';
+const IS_PRODUCT_SHOW_PRICE_SPECIAL = '<span class="sr-only visually-hidden">Orginele Prijs</span><s><span class="specialPrice">%s</span></s> <span class="sr-only visually-hidden">Verkoop prijs</span><span class="text-danger productPrice">%s</span>';
 const IS_PRODUCT_BUTTON_BUY = '<i class="fas fa-shopping-cart" title="Voeg toe aan de winkelwagen"></i>';
 const IS_PRODUCT_BUTTON_VIEW = '<i class="fas fa-eye" title="Bekijk artikel"></i> Bekijk';
