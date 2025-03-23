b8400b53edca00110d043fb1a94e2caa3745cba7
Fiber-cyber <mail@limshakei.nl>
2025-03-23 10:16:58 +0100

    Taal Verkenner Introductie
    Introductie Taal Verkenner

---
+++ admin/includes/languages/dutch/language_explorer.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2025 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const HEADING_TITLE = 'Taal Verkenner';
+
+const HEADING_TITLE_2 = 'Taal Verkenner [%s]';
+
+const TABLE_HEADING_FILE = 'Bestand';
+const TABLE_HEADING_ACTION = 'Actie';
+
+const FILE_COPIED_TO_TEMPLATE = 'Succes:  Het bestand <b>%s</b> is gekopieërd naar Template <b>%s</b>.<br><br>Het nieuwe bestand is hier te vinden <b>%s</b>';
+const FILE_DELETED_FROM_TEMPLATE = 'Het bestand <b>%s</b> is verwijderd van Template <b>%s</b>.';
+
+const ERROR_FILE_NOT_ACCEPTABLE = 'Bestand kan niet worden gekopieërd';
+
+const GET_HELP_LINK = 'https://phoenixcart.org/phoenixcartwiki/index.php?title=Change_Language_Files';
