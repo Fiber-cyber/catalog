c3eb4c9b989ff57641f974d7ad4e9f7b453028f9
Fiber-cyber <mail@limshakei.nl>
2025-06-02 12:50:47 +0200

    Update cm_t_write.php
    typo

--- includes/languages/dutch/modules/content/testimonials/cm_t_write.php
+++ includes/languages/dutch/modules/content/testimonials/cm_t_write.php
@@ -20,7 +20,7 @@
   const MODULE_CONTENT_TESTIMONIALS_WRITE_NICKNAME = 'Uw Naam';
   const MODULE_CONTENT_TESTIMONIALS_WRITE_NICKNAME_PLACEHOLDER = 'Uw naam (of bijnaam)';
   const MODULE_CONTENT_TESTIMONIALS_WRITE_TEXT = 'Aanbeveling';
-  const MODULE_CONTENT_TESTIMONIALS_WRITE_TEXT_PLACEHOLDER = 'Wat zou u wlillen schrijven?';
+  const MODULE_CONTENT_TESTIMONIALS_WRITE_TEXT_PLACEHOLDER = 'Wat zou u willen schrijven?';
   const MODULE_CONTENT_TESTIMONIALS_WRITE_BUTTON_TEXT_SEND = 'Verzonden naar Winkeleigenaar';
 
   const MODULE_CONTENT_TESTIMONIALS_WRITE_THANK_YOU = 'Dank u wel %s, Uw aanbeveling is verzonden naar de Winkeleigenaar.';
