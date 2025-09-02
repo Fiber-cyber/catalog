d7c2a50ee82c6fd163b78aa8e13e01ae8e7844dd
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2025-09-02 14:03:53 +0200

    Update d_version_check.php
    correctie van versie

--- admin/includes/languages/dutch/modules/dashboard/d_version_check.php
+++ admin/includes/languages/dutch/modules/dashboard/d_version_check.php
@@ -15,7 +15,7 @@ const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_DESCRIPTION = 'Toon de resultaten van
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_DATE = 'Laatst gecontroleerd op';
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CHECK_NOW = 'Check nu';
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_NEVER = 'Nooit';
-const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CURRENT = 'Uw huidige Phoenix Cart is %s';
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CURRENT = 'Uw huidige Phoenix Cart is v%s';
 
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_UPDATE_AVAILABLE = '<i class="fas fa-exclamation-circle"></i> Er is een update voor Phoenix Cart <b>v%s</b> beschikbaar!';
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_IS_LATEST = '<i class="fas fa-thumbs-up"></i> U gebruikt v%s, de laatste Phoenix Cart release!';
