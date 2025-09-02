934dee394fd811160997e87e27ad7c6761985647
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2025-09-02 18:07:31 +0200

    Update version_check.php

--- admin/includes/languages/dutch/version_check.php
+++ admin/includes/languages/dutch/version_check.php
@@ -16,15 +16,15 @@ const TABLE_HEADING_VERSION = 'Versie';
 const TABLE_HEADING_RELEASED = 'Datum van publicatie';
 const TABLE_HEADING_ACTION = 'Actie';
 
-const TITLE_INSTALLED_VERSION = 'Geïnstalleerde versie: <strong>CE Phoenix v%s</strong>';
+const TITLE_INSTALLED_VERSION = 'Geïnstalleerde versie: <strong>Phoenix cart v%s</strong>';
 
 const VERSION_SERVER_FAILURE = 'De beschikbare versies van de server kunnen niet worden geladen. Controleer uw internet verbinding of probeer het later opnieuw.';
-const VERSION_RUNNING_LATEST = 'U gebruikt de nieuwste versie van CE Phoenix.';
+const VERSION_RUNNING_LATEST = 'U gebruikt de nieuwste versie van Phoenix Cart.';
 const VERSION_UPGRADES_AVAILABLE = <<<'EOT'
-<strong>CE Phoenix %s</strong> is de laatste versie die voor u beschikbaar is!<hr>
+<strong>Phoenix Cart %s</strong> is de laatste versie die voor u beschikbaar is!<hr>
 <a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/viewforum.php?f=22">Gecertificeerde Partners</a>
  en gedetailleerde "<a class="alert-link" target="_blank" href="https://phoenixcart.org/forum/app.php/tag/phoenix-update">Doe het zelf</a>"
- instructies zijn beschikbaar in het Phoenix Forum.
+ instructies zijn beschikbaar in het Phoenix Cart Forum.
 EOT;
 
 const GET_HELP_LINK = 'https://phoenixcart.org/phoenixcartwiki/index.php?title=Version_Checker';
