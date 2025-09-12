282c256b13e4c5c23a769430686647c81f9f918c
Fiber-cyber <mail@limshakei.nl>
2025-09-12 13:19:43 +0200

    Introductie Pulse Analytics
    Pulse

---
+++ admin/includes/languages/dutch/pulse_analytics.php
@@ -0,0 +1,49 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2021 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const HEADING_TITLE = 'Pulse';
+
+const TEXT_DISPLAY_NUMBER_OF_EVENTS = 'Weergave van <b>%s</b> tot <b>%s</b> van <b>%s</b> evenementen';
+
+const TABLE_HEADING_EVENT_TYPE = 'Event Type';
+const TABLE_HEADING_PRODUCT_ID = 'Artikel ID';
+const TABLE_HEADING_CUSTOMER_ID = 'Klant ID';
+const TABLE_HEADING_CREATED_AT = 'Tijd';
+const TABLE_HEADING_ACTION = 'Actie';
+
+const TEXT_INFO_HEADING_DELETE_EVENT = 'Verwijder Evenement';
+const TEXT_INFO_DELETE_INTRO = 'Het verwijderen van dit evenement is definitief. Weet je het zeker?....';
+
+const BUTTON_PRODUCT_VIEW = 'Artikel Weergave';
+
+const BUTTON_FILTER = 'Filter';
+
+const JSON_NO_PAYLOAD = 'No Payload';
+const TEXT_INFO_NA = 'N/A';
+
+const PRODUCT_VIEW_CHOOSE_PRODUCT = '-- Selecteer een artikel --';
+const PRODUCT_VIEW_ALL_PRODUCTS = 'ALLE Artikelen';
+const PRODUCT_VIEW_CHOOSE_LABEL = 'Selecteer artikel';
+const PRODUCT_VIEW_START_DATE = 'Start Datum';
+const PRODUCT_VIEW_END_DATE = 'Eind Datum';
+
+const PRODUCT_VIEW_CHART_TITLE = 'Artikel Analytics';
+const PRODUCT_VIEW_CHART_TITLE_ALL = ' - Alle artikelen';
+const PRODUCT_VIEW_CHART_TITLE_DATE_RANGE = '%s t/m %s';
+
+const ERROR_INVALID_DATE_FORMAT = '<p>Ongeldig datum formaat</p>';
+
+
+ 
+
+
+
