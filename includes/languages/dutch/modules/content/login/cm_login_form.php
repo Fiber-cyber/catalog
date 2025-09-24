7cffe9a027e561663db3b6908144a75a02274fcb
Fiber-cyber <mail@limshakei.nl>
2025-09-24 12:28:22 +0200

    Update cm_login_form.php
    Klant Status

--- includes/languages/dutch/modules/content/login/cm_login_form.php
+++ includes/languages/dutch/modules/content/login/cm_login_form.php
@@ -16,7 +16,9 @@ const MODULE_CONTENT_LOGIN_FORM_DESCRIPTION = 'Toon een aanmeldformulier op de a
 const MODULE_CONTENT_LOGIN_HEADING_RETURNING_CUSTOMER = 'Terugkerende klant';
 const MODULE_CONTENT_LOGIN_TEXT_RETURNING_CUSTOMER = 'Ik ben een terugkerende klant.';
 
-const MODULE_CONTENT_LOGIN_TEXT_LOGIN_ERROR = 'Error: Geen match voor E-mailadres en/of wachtwoord.';
+const MODULE_CONTENT_LOGIN_TEXT_LOGIN_ERROR = 'Geen overeenkomst voor e-mailadres en/of wachtwoord. Neem <a href="%s" class="alert-link">contact met ons op</a> of probeer het opnieuw.';
+const MODULE_CONTENT_LOGIN_TEXT_LOGIN_NO_MATCH = 'Uw e-mailadres komt niet voor in onze administratie. <a href="%s" class="alert-link">Neem contact met ons op</a>.';
+const MODULE_CONTENT_LOGIN_TEXT_LOGIN_SUSPENDED = 'Uw account is opgeschort. Neem contact met ons op via <a href="%s" class="alert-link">contact</a> om uw account te herstellen.';
 
 const MODULE_CONTENT_LOGIN_ENTRY_EMAIL_ADDRESS_PLACEHOLDER = 'E-mail';
 const MODULE_CONTENT_LOGIN_ENTRY_PASSWORD_PLACEHOLDER = 'Wachtwoord';
