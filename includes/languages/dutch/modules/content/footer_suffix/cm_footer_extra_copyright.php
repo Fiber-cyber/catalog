19488f6d000d1eb79f8acf5c575da40f7f570fc6
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-03-08 13:44:40 +0100

    Update cm_footer_extra_copyright.php
    CE verwijderd

--- includes/languages/dutch/modules/content/footer_suffix/cm_footer_extra_copyright.php
+++ includes/languages/dutch/modules/content/footer_suffix/cm_footer_extra_copyright.php
@@ -13,5 +13,5 @@
   const MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_TITLE = 'Copyright Details';
   const MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_DESCRIPTION = 'Voeg een Copyright Blok toe aan de the Extra Footer van je shop';
   
-  const FOOTER_TEXT_BODY = '<p>Copyright &copy; %s <a class="text-body-emphasis" href="%s">%s</a> &middot; Powered by <a class="text-body-emphasis" href="https://phoenixcart.org" target="_blank" rel="noreferrer">CE Phoenix Cart</a></p>';
+  const FOOTER_TEXT_BODY = '<p>Copyright &copy; %s <a class="text-body-emphasis" href="%s">%s</a> &middot; Powered by <a class="text-body-emphasis" href="https://phoenixcart.org" target="_blank" rel="noreferrer">Phoenix Cart</a></p>';
  
