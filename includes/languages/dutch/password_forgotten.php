93fd2845fed7fee15578814bef55964a6aac3c36
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-03-14 03:27:59 +0100

    tekst aangepast
    tekst aangepast

--- includes/languages/dutch/password_forgotten.php
+++ includes/languages/dutch/password_forgotten.php
@@ -20,6 +20,7 @@ const TEXT_MAIN = 'Als u uw wachtwoord bent vergeten, vul hieronder uw e-mailadr
 const TEXT_PASSWORD_RESET_INITIATED = 'Controleer uw e-mail voor instructies over hoe u uw wachtwoord kunt wijzigen. De instructies bevatten een link die slechts 24 uur geldig is of totdat uw wachtwoord is bijgewerkt.';
 
 const TEXT_NO_EMAIL_ADDRESS_FOUND = 'Fout: het e-mailadres is niet gevonden in onze database, probeer het opnieuw.';
+const TEXT_NO_EMAIL_ADDRESS_FOUND = 'Als dit e-mail adres in onze bestanden staat, hebben we u een link gestuurd om uw account te resetten. Controleer uw inbox.';
 
 const EMAIL_PASSWORD_RESET_SUBJECT = STORE_NAME . ' - Nieuw wachtwoord';
 const EMAIL_PASSWORD_RESET_BODY = 'Er is een nieuw wachtwoord aangevraagd voor uw profiel op ' . STORE_NAME . '.' . "\n\n" . 'Volg deze persoonlijke link om uw wachtwoord veilig te wijzigen:' . "\n\n%s\n\n" . 'Deze link wordt automatisch verwijderd na 24 uur of nadat uw wachtwoord is gewijzigd.' . "\n\n" . 'Voor hulp bij een van onze online services kunt u een e-mail sturen naar de winkeleigenaar: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
