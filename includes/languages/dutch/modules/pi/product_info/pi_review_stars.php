9677e02d0ea1e8c7aa16af8bc1cb6f7c919ba1a7
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-04-12 10:55:57 +0200

    Introductie
    Introduceer lay-outmodules voor productinformatie
    Naam, Prijs, Link naar recensies, Voorraad

---
+++ includes/languages/dutch/modules/pi/product_info/pi_review_stars.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2026 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const PI_REVIEW_STARS_TITLE         = 'Beoordelings sterren & link';
+  const PI_REVIEW_STARS_DESCRIPTION   = 'Toont sterren, het aantal beoordelingen en een link om een ​​beoordeling te schrijven. <div class="alert alert-info">Dit is een submodule voor gebruik met het &pi;-systeem.</div>';
+  
+  const PI_REVIEW_STARS_COUNT = '%s Beoordeling';
+  const PI_REVIEW_STARS_COUNT_ONE = '%s Beoordelingen';
+
+  const PI_REVIEW_STARS_DO_REVIEW = 'Voeg uw beoordeling toe';
+
+  const PI_REVIEW_STARS_DO_FIRST_REVIEW = 'Schrijf de eerste beoordeling voor dit artikel.';
---
+++ includes/languages/dutch/modules/pi/product_info/pi_stock.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2026 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+  const PI_STOCK_TITLE        = 'Voorraad';
+  const PI_STOCK_DESCRIPTION  = 'Toont een bericht over de voorraad.<div class="alert alert-info">Dit is een submodule voor gebruik met het &pi;-systeem.</div>';
+
+  const PI_STOCK_AVAILABLE = '<span class="text-success fw-bold">Op Voorraad</span>';
+  const PI_STOCK_OOS = '<span class="text-danger">Niet op Voorraad</span>';
