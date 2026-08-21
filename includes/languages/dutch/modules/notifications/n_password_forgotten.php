96bdbe6ec2419faa5c921f2d5482f964f7a50639
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-08-21 07:35:09 +0200

    Wachtwoord vergeten update
    update wachtwoord vergeten naar notificatiemodule

---
+++ includes/languages/dutch/modules/notifications/n_password_forgotten.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+  Copyright (c) 2026 Phoenix Cart
+  Released under the GNU General Public License
+*/
+
+const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TITLE = 'Melding wachtwoord vergeten';
+const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_DESCRIPTION = 'Verstuurt een e-mail met de link om het wachtwoord opnieuw in te stellen.';
+
+const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TEXT_SUBJECT = STORE_NAME . ' - Wachtwoord opnieuw instellen - ';
+const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TEXT_BODY = 'We hebben een verzoek ontvangen om uw wachtwoord voor uw account opnieuw in te stellen. ' . STORE_NAME . '.' . "\n\n" . 'Klik op de onderstaande link om een ​​nieuw wachtwoord te kiezen:' . "\n\n%s\n\n" . 'Deze link is 24 uur geldig.' . "\n\n" . 'Heeft u hulp nodig? Neem contact met ons op via: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
