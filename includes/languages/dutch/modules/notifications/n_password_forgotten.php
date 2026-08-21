0845709c6c47f86251bebbd5b534d0b2bb8a0c31
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-08-21 09:39:05 +0200

    Update n_password_forgotten.php

--- includes/languages/dutch/modules/notifications/n_password_forgotten.php
+++ includes/languages/dutch/modules/notifications/n_password_forgotten.php
@@ -11,4 +11,4 @@ const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TITLE = 'Melding wachtwoord verget
 const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_DESCRIPTION = 'Verstuurt een e-mail met de link om het wachtwoord opnieuw in te stellen.';
 
 const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TEXT_SUBJECT = STORE_NAME . ' - Wachtwoord opnieuw instellen - ';
-const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TEXT_BODY = 'We hebben een verzoek ontvangen om uw wachtwoord voor uw account opnieuw in te stellen. ' . STORE_NAME . '.' . "\n\n" . 'Klik op de onderstaande link om een ​​nieuw wachtwoord te kiezen:' . "\n\n%s\n\n" . 'Deze link is 24 uur geldig.' . "\n\n" . 'Heeft u hulp nodig? Neem contact met ons op via: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
+const MODULE_NOTIFICATIONS_PASSWORD_FORGOTTEN_TEXT_BODY = 'We hebben een verzoek ontvangen om uw wachtwoord voor uw account opnieuw in te stellen van shop. ' . STORE_NAME . '.' . "\n\n" . 'Klik op de onderstaande link om een ​​nieuw wachtwoord te kiezen:' . "\n\n%s\n\n" . 'Deze link is 24 uur geldig.' . "\n\n" . 'Heeft u hulp nodig? Neem contact met ons op via: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
