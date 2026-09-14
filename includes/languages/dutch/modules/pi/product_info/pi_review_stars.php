34aebf5772f9fc6bea79263ec7f2343474340a1f
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-09-14 13:52:02 +0200

    Update pi_review_stars.php
    vergeten const

--- includes/languages/dutch/modules/pi/product_info/pi_review_stars.php
+++ includes/languages/dutch/modules/pi/product_info/pi_review_stars.php
@@ -5,13 +5,19 @@
   CE Phoenix, E-Commerce made Easy
   https://phoenixcart.org
 
-  Copyright (c) 2024 Phoenix Cart
+  Copyright (c) 2026 Phoenix Cart
 
   Released under the GNU General Public License
 */
 
+  const PI_REVIEW_STARS_TITLE         = 'Review Sterren & Link';
+  const PI_REVIEW_STARS_DESCRIPTION   = 'Toont sterren, het aantal recensies en een link om een ​​recensie te schrijven.<div class="alert alert-info">Dit is een submodule voor gebruik met het &pi; systeem.</div>';
+
   const PI_REVIEW_STARS_COUNT = '%s Beoordelingen';
   const PI_REVIEW_STARS_COUNT_ONE = '%s Beoordeling';
+  
+  const PI_REVIEW_STARS_DO_REVIEW = 'Schrijf een beoordeling';
+  
   const PI_REVIEW_STARS_DO_FIRST_REVIEW = 'Schrijf de eerste beoordeling voor dit artikel.';
   
   
