2c7333334b4ba6b2df711e27213b5f788c0fe4be
Fiber <64646021+Fiber-cyber@users.noreply.github.com>
2026-09-20 11:03:00 +0200

    Update pi_name.php
    Vergeten schijnbaar (-:

--- includes/languages/dutch/modules/pi/product_info/pi_name.php
+++ includes/languages/dutch/modules/pi/product_info/pi_name.php
@@ -10,4 +10,8 @@
   Released under the GNU General Public License
 */
 
-const PI_NAME_DISPLAY_NAME = '%s';
+  const PI_NAME_TITLE         = 'Naam';
+  const PI_NAME_DESCRIPTION   = 'Toont de productnaam.<div class="alert alert-info">Dit is een submodule voor gebruik met het &pi; systeem.</div>';
+
+  const PI_NAME_DISPLAY_NAME = '%s';
+
